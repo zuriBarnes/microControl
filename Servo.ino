d909777d812f608042d2bd6cfa83e6b5c642acd5
Zuri <45982373+zuriBarnes@users.noreply.github.com>
2021-06-08 22:17:05 -0400

    update to servo notes

--- Servo.ino
+++ Servo.ino
@@ -1,14 +1,14 @@
 #include <Servo.h>
 
 // Servo values
-Servo myServo;
+Servo myServo; // new Servo object
 int servoDelay = 1000;
 int servoMax = 180;
 int servoMin = 0;
 
 
 void setup() {
-  myServo.attach(8);
+  myServo.attach(8); // attach to pin
 }
 
 void loop() {
