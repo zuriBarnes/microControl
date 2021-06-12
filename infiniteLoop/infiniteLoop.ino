5fc02f4c06265ac324cce9ba1e6c82b3ca526935
Zuri <45982373+zuriBarnes@users.noreply.github.com>
2021-06-11 20:48:19 -0400

    Update infiniteLoop.ino
    removed for loop

--- infiniteLoop/infiniteLoop.ino
+++ infiniteLoop/infiniteLoop.ino
@@ -1,19 +1,7 @@
 void setup() {
   Serial.begin(9600);
-  for(int i; i <= 12; i++) {
-      pinMode(i, OUTPUT);
     }
-
-}
 
 void loop() {
-  for(int i; i <= 12; i++) {
-    digitalWrite(i, HIGH);
-    delay(1000);
-    digitalWrite(i, LOW);
-    delay(1000);
-    
-    }
 
-  
 }
