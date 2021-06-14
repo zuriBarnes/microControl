d8956543e68dd0456d47b838ea7918737f8bbdec
Zuri <45982373+zuriBarnes@users.noreply.github.com>
2021-06-14 16:06:41 -0400

    Add C++ input/output file

---
+++ coutCin.cpp
@@ -0,0 +1,23 @@
+// C++ program to illustrate the take
+// multiple input
+#include <iostream>
+using namespace std;
+  
+// Driver Code
+int main()
+{
+    string name;
+    int age;
+  
+    // Take multiple input using cin
+    cout << "Enter your name below"<< endl;
+    cin >> name;
+    cout << "Enter your age  below"<< endl;
+    cin >> age;
+  
+    // Print output
+    cout << "Name : " << name << endl;
+    cout << "Age : " << age << endl;
+  
+    return 0;
+}
