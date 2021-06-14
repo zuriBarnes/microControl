88f66658fce928fc7ea58fa73d44da5c5ab19994
Zuri <45982373+zuriBarnes@users.noreply.github.com>
2021-06-14 16:37:52 -0400

    servo, and buzzer added

--- ultrasonic-sensor/ultrasonic-sensor.ino
+++ ultrasonic-sensor/ultrasonic-sensor.ino
@@ -1,4 +1,12 @@
+#include <Servo.h>
+
 // ========= Declarations ============ //
+// Servo values
+Servo myServo;
+int servoDelay = 100;
+int servoMax = 180;
+int servoMin = 0;
+
 
 // UltraSensor values
 const int trigPin = 5;
@@ -6,24 +14,35 @@ const int echoPin = 6;
 long duration;
 int distance;
 
+// Buzzer
+const int buzzPin = 3;
+
 // LED values
 const int redLED = 11;
 const int blueLED = 10;
 const int greenLED = 9;
 
 void setup() { // =========== SETUP ========= //
+
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(redLED, OUTPUT);
   pinMode(blueLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
+  pinMode(buzzPin, OUTPUT);
+  
 }
 
 void loop() { // =========== LOOP ========= //
+  
+
    motionSensor();
    if(distance <= 20) {
     rgb_LED(55,55,0);
+      digitalWrite(buzzPin, HIGH);
+        delay(100);
+       digitalWrite(buzzPin, LOW);
    } else {
       rgb_LED(0,0,0);
     }
@@ -31,6 +50,8 @@ void loop() { // =========== LOOP ========= //
 }
 
 //======= Functions ============ //
+
+
 void motionSensor() {
   digitalWrite(trigPin, LOW); // why this is needed?  
   delayMicroseconds(2);
